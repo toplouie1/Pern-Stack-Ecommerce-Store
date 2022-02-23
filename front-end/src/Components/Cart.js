facb08fa9f599d34a4e5ebe824c2059794dce41d
Nima sherpa <Toplouie78@gmail.com>
2022-02-23 12:53:47 -0500

    added the css for cart

--- front-end/src/Components/Cart.js
+++ front-end/src/Components/Cart.js
@@ -1,20 +1,49 @@
 import React from "react";
+import "./Cart.css";
+import Button from "@mui/material/Button";
 
 function Cart({ cartItems }) {
+	const buyAll = () => {
+		alert("THIS IS NOT A REAL PRODUCT ");
+	};
 	return (
 		<div className="cartItems">
 			<div className="cartHeader">Cart Items</div>
+			<Button
+				className="cartHeader"
+				size="small"
+				variant="outlined"
+				color="error"
+			>
+				Remove All
+			</Button>
 			{cartItems.length === 0 && (
 				<div className="emptyItems"> No items are added.</div>
 			)}
-			<div>
+			<div className="cartInfo">
 				{cartItems.map((item) => (
-					<div key={item.id} className="cartItems">
-						<div>{item.name}</div>
-						<img className="cartImg" src={item.image} alt={item.name} />
+					<div className="allCartItem">
+						<div key={item.id} className="cartItems">
+							<img className="cartImg" src={item.image} alt={item.name} />
+						</div>
+						<div>
+							<div>Name: {item.name}</div>
+							<div>Price: ${item.price}</div>
+							<Button size="small" variant="outlined" color="error">
+								Remove
+							</Button>
+						</div>
 					</div>
 				))}
 			</div>
+			<Button
+				onClick={buyAll}
+				className="cartButton"
+				size="small"
+				variant="contained"
+			>
+				Buy
+			</Button>
 		</div>
 	);
 }
